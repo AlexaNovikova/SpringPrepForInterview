0bff4723f7e1532e1a5839884e0f0ab08313f7e4
AlexNovikova <alexadubinina@mail.ru>
2021-10-29 14:55:03 +0700

    start

---
+++ src/main/java/ru/gb/StudentsInfo/StudentsInfoApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.StudentsInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsInfoApplication.class, args);
+	}
+
+}
